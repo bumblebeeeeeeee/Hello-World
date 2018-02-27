b0afc9dc3b31e1991d455577af03ee34a088d488
bumblebeeeeeeee <greenapple11365@gmail.com>
2018-02-26 22:10:57 -0500

    add space between sentences

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
     	for (int i = 0; i < 100; i++) {
-    		System.out.print("Hello, World!");
+    		System.out.print("Hello, World! ");
     	}
         
     }
